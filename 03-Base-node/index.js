de589eec9588af675cfc25d86027a286eea1da55
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-06-28 18:10:39 -0300

    Carpeta Base Node 2 (fs)

--- 03-Base-node/index.js
+++ 03-Base-node/index.js
@@ -1,6 +1,24 @@
-// Hacer tabla del 5 e imprimir en la consola
+const fs = require("fs");
 
-const base = 5
+/*Este código importa el módulo fs de la 
+biblioteca estándar de Node.js y lo 
+asigna a una variable constante llamada fs. 
+El módulo fs proporciona funciones para 
+interactuar con el sistema de archivos en 
+JavaScript.
+*/
+
+//? Hacer tabla del 5 e imprimir en la consola
+
+const base = 5;
+let salida = "";
+
 for (let i = 1; i <= 10; i++) {
   console.log(`${base} * ${i} = ${base * i}`);
 }
+
+fs.writeFile("tabla.txt", salida, (err) => {
+  if (err) throw err;
+
+  console.log("Archivo tabla.txt creado");
+});
