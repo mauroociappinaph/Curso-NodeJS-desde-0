1a3baca014eff319eac1fc7f5bbbcf242b5b6771
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-06-28 15:17:10 -0300

    Archivo 03

---
+++ 01-hola-mundo/app3.js
@@ -0,0 +1,13 @@
+console.log("Inicio del programa");
+
+setTimeout(() => {
+  console.log("Primer TimeOut");
+}, 3000);
+setTimeout(() => {
+  console.log("Segundo TimeOut");
+}, 0);
+setTimeout(() => {
+  console.log("Tercer TimeOut");
+}, 0);
+
+console.log("Fin del programa");
