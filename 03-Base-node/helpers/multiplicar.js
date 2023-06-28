58883614bbe96be2f8fab5437b4ccf827f174c6a
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-06-28 18:46:31 -0300

    Carpeta HELPERS(2)

--- 03-Base-node/helpers/multiplicar.js
+++ 03-Base-node/helpers/multiplicar.js
@@ -1,19 +1,24 @@
+const fs = require('fs');
+
 const crearArchivo = (base = 5) => {
-    let salida= "";
-    for (let i = 1; i <= 10; i++) {
-        salida += `${base} * ${i} = ${base * i}\n`;
-        console.log(`${base} * ${i} = ${base * i}`);
-    }
-    console.log(salida);
-    try {
-        fs.writeFileSync("tabla.txt", salida);
-        console.log("tabla.txt creado");
-    } catch (err) {
-        console.error("Error al escribir en tabla.txt:", err);
-    }
+    return new Promise((resolve, reject) => {
+        console.log("==========================");
+        console.log(" Tabla del ", base);
+        console.log("==========================");
 
-    }
-    crearArchivo();
+        let salida = "";
 
+        for (let i = 1; i <= 10; i++) {
+            salida += `${base} * ${i} = ${base * i}\n`;
+        }
 
+        console.log(salida);
 
+        fs.writeFileSync(`tabla-${base}.txt`, salida);
+        resolve(`tabla-${base}.txt`);
+    });
+};
+
+module.exports = {
+    crearArchivo
+};
--- 03-Base-node/index.js
+++ 03-Base-node/index.js
@@ -1,18 +1,8 @@
-const fs = require("fs");
+const { crearArchivo } = require("./helpers/multiplicar");
 
-const base = 5;
-let salida = "";
+console.clear();
+const base = 3;
 
-for (let i = 1; i <= 10; i++) {
-  salida += `${base} * ${i} = ${base * i}\n`;
-  console.log(`${base} * ${i} = ${base * i}`);
-}
-
-console.log(salida);
-
-try {
-  fs.writeFileSync("tabla.txt", salida);
-  console.log("tabla.txt creado");
-} catch (err) {
-  console.error("Error al escribir en tabla.txt:", err);
-}
+crearArchivo(base)
+    .then(nombreArchivo => console.log(nombreArchivo, "creado"))
+    .catch(err => console.log(err));
